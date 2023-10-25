725afc7ada79e9edaac24b883d00959a8fab45b1
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-10-25 20:16:42 -0300

    El Evade tiene rango

--- Assets/Scripts/Try/EvadeTarget.cs
+++ Assets/Scripts/Try/EvadeTarget.cs
@@ -11,15 +11,33 @@ public class EvadeTarget : MonoBehaviour
     [SerializeField]
     bool seek;
 
+    [SerializeField]
+    float evadeRadius = 5f;
+
     private void Update()
     {
-        var force = agent.Evade(evade);
-
-        if (seek)
+        if (IsWithinRadius(evade.transform.position, transform.position, evadeRadius))
         {
-            force += agent.Seek(evade.transform.position);
-        }
+            var force = agent.Evade(evade);
 
-        agent.Accelerate(force);
+            if (seek)
+            {
+                force += agent.Seek(evade.transform.position);
+            }
+
+            agent.Accelerate(force);
+        }
+    }
+
+    private bool IsWithinRadius(Vector3 position1, Vector3 position2, float radius)
+    {
+        // Comprueba si la distancia entre position1 2 es menor o igual al radio
+        return Vector3.Distance(position1, position2) <= radius;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, evadeRadius);
     }
 }
