742890a977d6b99dc4cd3fb15106d94e232c3516
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-11-13 12:58:02 -0300

    Movimiento y camara

---
+++ Assets/Scripts/Parcial 2/CamaraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour
+{
+
+    [SerializeField] Transform target; // target es el character
+    Vector3 offset; //diferencia de camara y player
+    [SerializeField] Vector3 offsetPublico; // cambio publico a la camara
+    Vector3 final;
+    //[SerializeField] float lerpspeed = 1;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+
+        transform.position = target.transform.position + offset + offsetPublico;
+
+    }
+
+}
---
+++ Assets/Scripts/Parcial 2/Character.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    private Rigidbody myRig;
+    private float yVelocity;
+    public float speed = 200f;
+
+    private void Awake()
+    {
+        myRig = GetComponent<Rigidbody>(); //CAMBIAR
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        Vector3 horizontalMove = moveDirection * speed * Time.deltaTime;
+        myRig.velocity = new Vector3(horizontalMove.x, myRig.velocity.y, horizontalMove.z);
+    }
+}
