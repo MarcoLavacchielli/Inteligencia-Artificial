fae3a99ca9abb690d46ed64934d764431000d242
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-11-14 15:00:49 -0300

    Character provisorio

---
+++ Assets/Scripts/Parcial 2/Character.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    Animator animator;
+
+    public Vector3 velocity;
+
+
+    private void Update()
+    {
+        animator.SetFloat("_MoveSpeed", velocity.magnitude);
+        animator.SetFloat("_DirX", velocity.normalized.x);
+        animator.SetFloat("_DirY", velocity.normalized.z);
+
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    public void Talk()
+    {
+        velocity = Vector3.zero;
+        animator.SetTrigger("_Talk");
+    }
+}
--- Assets/Scripts/Parcial 2/Player/CharacterPlayer.cs
+++ Assets/Scripts/Parcial 2/Player/CharacterPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public class CharacterPlayer : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody myRig;
