0f80ed4d624b64b7178e96a1e2e2f26278c55faf
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-11-25 13:23:41 -0300

    No lo se

--- Assets/Scripts/Parcial 2/Player/Character.cs
+++ Assets/Scripts/Parcial 2/Player/Character.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +5,9 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     //[SerializeField] Animator animator;
-
     public Vector3 velocity;
 
+    private Node currentNode; // Nodo actual debajo del jugador
 
     private void Update()
     {
@@ -18,9 +17,37 @@ public class Character : MonoBehaviour
         //animator.SetFloat("_DirY", velocity.normalized.z);
 
         transform.position += velocity * Time.deltaTime;
-        if(velocity.sqrMagnitude > 0)
+        if (velocity.sqrMagnitude > 0)
         {
             transform.forward = velocity.normalized;
+        }
+
+        // Verificar si el jugador está sobre un nodo
+        CheckNodeUnderPlayer();
+    }
+
+    private void CheckNodeUnderPlayer()
+    {
+        // Obtener el nodo debajo del jugador usando el script PhysicalNodeGrid
+        Node nodeUnderPlayer = PhysicalNodeGrid.Instance.GetClosest(transform.position);
+
+        // Verificar si el nodo ha cambiado
+        if (nodeUnderPlayer != currentNode)
+        {
+            // Restaurar el color del nodo anterior
+            if (currentNode != null)
+            {
+                currentNode.SetColor(Color.white);
+            }
+
+            // Cambiar el color del nuevo nodo
+            if (nodeUnderPlayer != null)
+            {
+                nodeUnderPlayer.SetColor(Color.green);
+            }
+
+            // Actualizar el nodo actual
+            currentNode = nodeUnderPlayer;
         }
     }
 
