fd59ad89d4ffd87297483c3a90ef5de050121008
Marco <marco.lavacchielli@davinci.edu.ar>
2023-11-26 15:40:04 -0300

    mas cosas inecesarias

--- Assets/Scripts/Parcial 2/Clases/DesicionAI.cs
+++ Assets/Scripts/Parcial 2/Clases/DesicionAI.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class DesicionAI : MonoBehaviour
 {
