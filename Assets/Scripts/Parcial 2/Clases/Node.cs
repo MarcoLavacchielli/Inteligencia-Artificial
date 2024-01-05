30c279f1f078074ff2bd64501b00592f1ce40921
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2024-01-05 17:39:48 -0300

    mini avance

--- Assets/Scripts/Parcial 2/Clases/Node.cs
+++ Assets/Scripts/Parcial 2/Clases/Node.cs
@@ -269,6 +269,18 @@ public class Node : MonoBehaviour
         return new List<Node>();
     }
 
+    public void ChangeNodeColor(Color color)
+    {
+        this.color = color;
+
+        if (TryGetComponent<MeshRenderer>(out var rend))
+        {
+            var block = new MaterialPropertyBlock();
+            block.SetColor("_Color", color);
+            rend.SetPropertyBlock(block);
+        }
+    }
+
 }
 
 
